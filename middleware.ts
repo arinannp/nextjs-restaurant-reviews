271be0054e9605a03dace58310250d54d67d2504
Arinan <arinannp@gmail.com>
2025-07-31 12:27:35 +0700

    fix: next auth session in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+// import { NextResponse } from "next/server"
+// import type { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 
 export async function middleware(req: NextRequest) {
